3e0cd3a0f790ec12589804aeae0afd362b6ba60f
Kyle Baker <kydoggcodes@gmail.com>
2026-01-11 15:40:47 -0500

    Initial Xcode project setup with assets

---
+++ PawConnect/PawConnect/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PawConnect
+//
+//  Created by Kyle Baker on 1/11/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
